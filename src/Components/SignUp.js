2e2fa21bc0fd217c024644e1a09b63710c28e367
Baubs <pathogenic@gmail.com>
2022-05-09 22:29:17 -0500

    more registration work

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -6,7 +6,10 @@ const SignUp = () => {
     const [auth, setAuth] = useState();
     const [signedIn, setSignedIn] = useState();
 
+    
     useEffect(() => {
+        
+        
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                 clientId: Secrets.CLIENTID,
@@ -15,26 +18,29 @@ const SignUp = () => {
                 //this.auth = window.gapi.auth2.getAuthInstance();
                 setAuth(window.gapi.auth2.getAuthInstance());
                 setSignedIn(auth.isSignedIn.get());
+                //console.log(auth.currentUser.get.getBasicProfile());
             });
         });
+        
     }, [])
 
     const onSignInClick = async () => {
         await auth.signIn();
         let id_token = auth.currentUser.get().getAuthResponse().id_token;
         
-        backEnd.post('/SignIn', 'idtoken=' + id_token, {
+        const response = await backEnd.post('/register', 'idtoken=' + id_token, {
             headers: {
                 'Content-Type' : 'application/x-www-form-urlencoded'
             }
         })
+        //redirect to Registration component here to ask for extra info once more is added
         setSignedIn(auth.isSignedIn.get());
-        console.log(signedIn);
+        console.log(response);
         
     };
 
     const onSignOutClick = async () => {
-      await auth.signOut();
+        await auth.signOut();
         setSignedIn(auth.isSignedIn.get());
     }
 
